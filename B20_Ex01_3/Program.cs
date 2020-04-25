a63be4466cf979c582080be97b1d4767b5c294f9
noamce <noam248@gmail.com>
2020-04-25 21:39:54 +0300

    ex_3 done and fix 2

---
+++ B20_Ex01_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace B20_Ex01_3
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            int hourGlassHeight = getUserInput();
+            B20_Ex01_2.Program.PrintHourGlassOfAsterisks(hourGlassHeight);
+        }
+
+        private static int getUserInput()
+        {
+            string userInput;
+            int hourGlassHeight;
+            bool isInputValid = false;
+
+            do
+            {
+                Console.WriteLine("Please Enter the desired hour glass height");
+                userInput = Console.ReadLine();
+                isInputValid = inputValidation(userInput, out hourGlassHeight);
+                if (isInputValid == false)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+            while (isInputValid == false);
+
+            return hourGlassHeight;
+        }
+
+        private static bool inputValidation(string i_Input, out int o_HourGlassHeight)
+        {
+            return int.TryParse(i_Input, out o_HourGlassHeight);
+        }
+    }
+}
+
